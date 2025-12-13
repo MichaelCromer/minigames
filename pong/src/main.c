aca876b1a4411186c79378e813e430fd35247074
Michael Cromer <mcromer@proton.me>
2025-12-13 16:19:15 +1100

    Pong clean up const names

--- pong/src/main.c
+++ pong/src/main.c
@@ -4,47 +4,23 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-/*
- *  title screen
- *  1p mode
- *  2p mode (?)
- *
- *  player can 'jump' to whack ball
- *
- *  generically :
- *      pong_input
- *          captures keypresses etc, parses/interprets it, and stores data
- *      state_update
- *          updates global state available to all
- *      player1_update, namespace_update etc
- *          use captured input data to decide updates
- *
- *  is that too complex for such a simpe game?
- *
- */
-
-
-const int window_w = 800;
-const int window_h = 600;
-
-const int paddle_h = 90;
-const int paddle_w = 6;
-const float paddle_speed = 360.0f;
+const int WINDOW_W = 800;
+const int WINDOW_H = 600;
+const float BALL_SPEED = 360.0f;
+const float BALL_RADIUS = 6.0f;
+const int PADDLE_H = 90;
+const int PADDLE_W = 6;
+const float PADDLE_SPEED = 360.0f;
+const float AI_LOOKAHEAD_SEC = 0.3f;
 
 enum PLAYER_MOVE { NONE, UP, DOWN };
 struct Player { Vector2 pos; enum PLAYER_MOVE dir; };
+
 struct Player player1 = { .pos = { 0.0f, 0.0f }, .dir = NONE };
 struct Player player2 = { .pos = { 0.0f, 0.0f }, .dir = NONE };
 
-float ai_lookahead_sec = 0.3f;
-int ai_key_press = 0;
-float ai_keypress_duration_min = 0.1f;
-
 Vector2 ball_p = { 0.0f, 0.0f };
 Vector2 ball_v = { 0.0f, 0.0f };
-float ball_speed = 360.0f;
-float ball_r = 6.0f;
-
 bool ball_is_out = false;
 
 
@@ -55,15 +31,15 @@ void update_player(struct Player *player, float dt)
 {
     switch (player->dir) {
         case UP:
-            player->pos.y -= paddle_speed * dt;
+            player->pos.y -= PADDLE_SPEED * dt;
             break;
         case DOWN:
-            player->pos.y += paddle_speed * dt;
+            player->pos.y += PADDLE_SPEED * dt;
             break;
         default:
             break;
     }
-    player->pos.y = Clamp(player->pos.y, 0, window_h - paddle_h);
+    player->pos.y = Clamp(player->pos.y, 0, WINDOW_H - PADDLE_H);
 }
 
 
@@ -71,26 +47,28 @@ void update_ball(float dt)
 {
     ball_p = Vector2Add(ball_p, Vector2Scale(ball_v, dt));
 
-    if ((ball_p.y < 0) || (ball_p.y > window_h)) ball_v.y *= -1;
-    if ((ball_p.x < ball_r) || (ball_p.x > window_w - ball_r)) ball_is_out = true;
+    if ((ball_p.y < 0) || (ball_p.y > WINDOW_H)) ball_v.y *= -1;
+    if (
+        (ball_p.x < BALL_RADIUS) || (ball_p.x > WINDOW_W - BALL_RADIUS)
+    ) ball_is_out = true;
 }
 
 
 void update_collisions(void)
 {
-    if (ball_p.x <= player1.pos.x + paddle_w + ball_r) {
-        if ((ball_p.y < player1.pos.y) || (ball_p.y > player1.pos.y + paddle_h)) return;
+    if (ball_p.x <= player1.pos.x + PADDLE_W + BALL_RADIUS) {
+        if ((ball_p.y < player1.pos.y) || (ball_p.y > player1.pos.y + PADDLE_H)) return;
         ball_v.x *= -1;
     }
-    if (ball_p.x >= player2.pos.x - ball_r) {
-        if ((ball_p.y < player2.pos.y) || (ball_p.y > player2.pos.y + paddle_h)) return;
+    if (ball_p.x >= player2.pos.x - BALL_RADIUS) {
+        if ((ball_p.y < player2.pos.y) || (ball_p.y > player2.pos.y + PADDLE_H)) return;
         ball_v.x *= -1;
     }
 
     ball_p.x = Clamp(
             ball_p.x,
-            player1.pos.x + paddle_w + ball_r + EPSILON,
-            player2.pos.x - ball_r - EPSILON
+            player1.pos.x + PADDLE_W + BALL_RADIUS + EPSILON,
+            player2.pos.x - BALL_RADIUS - EPSILON
     );
 }
 
@@ -100,13 +78,13 @@ void update_collisions(void)
 
 void draw_player(struct Player *player)
 {
-    DrawRectangle(player->pos.x, player->pos.y, paddle_w, paddle_h, WHITE);
+    DrawRectangle(player->pos.x, player->pos.y, PADDLE_W, PADDLE_H, WHITE);
 }
 
 
 void draw_ball(void)
 {
-    DrawCircle(ball_p.x, ball_p.y, ball_r, WHITE);
+    DrawCircle(ball_p.x, ball_p.y, BALL_RADIUS, WHITE);
 }
 
 
@@ -118,20 +96,20 @@ void pong_reset(void)
     ball_is_out = false;
 
     player1 = (struct Player) {
-        .pos = (Vector2) { 6.0f, (window_h - paddle_h) / 2 },
+        .pos = (Vector2) { 6.0f, (WINDOW_H - PADDLE_H) / 2 },
         .dir = NONE
     };
     player2 = (struct Player) {
-        .pos = (Vector2) { (window_w - 6.0f - paddle_w), (window_h - paddle_h) / 2 },
+        .pos = (Vector2) { (WINDOW_W - 6.0f - PADDLE_W), (WINDOW_H - PADDLE_H) / 2 },
         .dir = NONE
     };
 
-    ball_p = (Vector2){ window_w / 2, window_h / 2 };
+    ball_p = (Vector2){ WINDOW_W / 2, WINDOW_H / 2 };
 
     float theta = GetRandomValue(0, 360);
     ball_v = (Vector2){
-        ball_speed * cosf(theta),
-        ball_speed * sinf(theta)
+        BALL_SPEED * cosf(theta),
+        BALL_SPEED * sinf(theta)
     };
 }
 
@@ -150,16 +128,16 @@ void pong_input(void)
 
 void pong_ai(void)
 {
-    if ((ball_v.x < 0) || ((window_w - ball_p.x) / ball_v.x > ai_lookahead_sec)) {
+    if ((ball_v.x < 0) || ((WINDOW_W - ball_p.x) / ball_v.x > AI_LOOKAHEAD_SEC)) {
         player2.dir = NONE;
         return;
     }
 
-    if ((player2.pos.y + 2*paddle_h/3) < ball_p.y) {
+    if ((player2.pos.y + 2*PADDLE_H/3) < ball_p.y) {
         player2.dir = DOWN;
         return;
     }
-    if ((player2.pos.y + paddle_h/3) > ball_p.y) {
+    if ((player2.pos.y + PADDLE_H/3) > ball_p.y) {
         player2.dir = UP;
         return;
     }
@@ -197,7 +175,7 @@ void pong_draw(void)
 
 void pong_initialise(void)
 {
-    InitWindow(window_w, window_h, "pong");
+    InitWindow(WINDOW_W, WINDOW_H, "pong");
     SetExitKey(KEY_Q);
     SetRandomSeed(1 + GetMouseX()*GetMouseX() + GetMouseY()*GetMouseY());
     pong_reset();

6832b40304102de6593c9ea8f40651a8677cc59b
Michael Cromer <mcromer@proton.me>
2025-12-13 13:02:40 +1100

    Pong add ball kinematics

--- pong/src/main.c
+++ pong/src/main.c
@@ -6,7 +6,9 @@
 /*
  *  title screen
  *  1p mode
- *  2p mode
+ *  2p mode (?)
+ *
+ *  player can 'jump' to whack ball
  *
  *  generically :
  *      input_parse
@@ -16,6 +18,7 @@
  *      player_update, namespace_update etc
  *          use captured input data to decide updates
  *
+ *  is that too complex for such a simpe game?
  *
  */
 
@@ -23,65 +26,91 @@
 const int window_w = 800;
 const int window_h = 600;
 
-
-const int paddle_height = 60;
-const int paddle_width = 6;
+const int paddle_h = 90;
+const int paddle_w = 6;
 const float paddle_speed = 360.0f;
-
 
 enum PLAYER_MOVE { NONE, UP, DOWN };
 enum PLAYER_MOVE player_move = NONE;
-float player_y = 0.0f;
 
-Vector2 ball_pos = { 0.0f, 0.0f };
-Vector2 ball_velocity = { 0.0f, 0.0f };
-float ball_radius = 6.0f;
+Vector2 player_p = { 0.0f, 0.0f };
+
+Vector2 ball_p = { 0.0f, 0.0f };
+Vector2 ball_v = { 0.0f, 0.0f };
+float ball_speed = 360.0f;
+float ball_r = 6.0f;
+
+bool ball_is_out = false;
 
 
-/* player fns */
+/* update fns */
 
 
-void player_update(float dt)
+void update_player(float dt)
 {
     switch (player_move) {
         case UP:
-            player_y -= paddle_speed * dt;
+            player_p.y -= paddle_speed * dt;
             break;
         case DOWN:
-            player_y += paddle_speed * dt;
+            player_p.y += paddle_speed * dt;
             break;
         default:
             break;
     }
-    player_y = Clamp(player_y, 0, window_h - paddle_height);
+    player_p.y = Clamp(player_p.y, 0, window_h - paddle_h);
+}
+
+
+void update_ball(float dt)
+{
+    ball_p = Vector2Add(ball_p, Vector2Scale(ball_v, dt));
+
+    if ((ball_p.y < 0) || (ball_p.y > window_h)) ball_v.y *= -1;
+    if ((ball_p.x < 0) || (ball_p.x > window_w)) ball_is_out = true;
+}
+
+
+void update_collisions(void)
+{
+    if ((ball_p.y < player_p.y) || (ball_p.y > player_p.y + paddle_h)) return;
+    if (ball_p.x > player_p.x + paddle_w + ball_r) return;
+    ball_v.x *= -1;
 }
 
 
 /* draw fns */
 
 
-void draw_paddle(int x, int y)
+void draw_paddle(Vector2 p)
 {
-    DrawRectangle(x, y, paddle_width, paddle_height, WHITE);
+    DrawRectangle(p.x, p.y, paddle_w, paddle_h, WHITE);
 }
 
 
-void draw_ball(Vector2 pos)
+void draw_ball(void)
 {
-    DrawCircle(pos.x, pos.y, ball_radius, WHITE);
+    DrawCircle(ball_p.x, ball_p.y, ball_r, WHITE);
 }
 
 
 /* pong fns */
 
 
-void pong_initialise(void)
+void pong_reset(void)
 {
-    player_y = (window_h - paddle_height) / 2;
-    ball_pos = (Vector2){ window_w / 2, window_h / 2 };
+    static int seed = 0;
+    ball_is_out = false;
 
-    InitWindow(window_w, window_h, "pong");
-    SetExitKey(KEY_Q);
+    player_p = (Vector2) { 6.0f, (window_h - paddle_h) / 2 };
+    ball_p = (Vector2){ window_w / 2, window_h / 2 };
+
+    SetRandomSeed(seed++);
+    float theta = GetRandomValue(0, 360);
+    ball_v = (Vector2){
+        ball_speed * cosf(theta),
+        ball_speed * sinf(theta)
+    };
 }
 
 
@@ -101,7 +130,11 @@ void pong_update()
 {
     float dt = GetFrameTime();
     input_parse();
-    player_update(dt);
+    update_player(dt);
+    update_ball(dt);
+
+    update_collisions();
+    if (ball_is_out) pong_reset();
 }
 
 
@@ -110,10 +143,18 @@ void pong_draw(void)
     BeginDrawing();
     ClearBackground(SKYBLUE);
 
-    draw_paddle(6, player_y);
-    draw_ball(ball_pos);
+    draw_paddle(player_p);
+    draw_ball();
 
     EndDrawing();
+}
+
+
+void pong_initialise(void)
+{
+    InitWindow(window_w, window_h, "pong");
+    SetExitKey(KEY_Q);
+    pong_reset();
 }
 
 

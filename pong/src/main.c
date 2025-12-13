64ae2cdd0f4c6b0393ba0542a13c837cdc1b4737
Michael Cromer <mcromer@proton.me>
2025-12-13 15:25:57 +1100

    Pong player struct and player 2

--- pong/src/main.c
+++ pong/src/main.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include <raylib.h>
 #include <raymath.h>
 #include <stdbool.h>
@@ -15,7 +16,7 @@
  *          captures keypresses etc, parses/interprets it, and stores data
  *      state_update
  *          updates global state available to all
- *      player_update, namespace_update etc
+ *      player1_update, namespace_update etc
  *          use captured input data to decide updates
  *
  *  is that too complex for such a simpe game?
@@ -31,9 +32,13 @@ const int paddle_w = 6;
 const float paddle_speed = 360.0f;
 
 enum PLAYER_MOVE { NONE, UP, DOWN };
-enum PLAYER_MOVE player_move = NONE;
+enum PLAYER_MOVE player1_move = NONE;
+enum PLAYER_MOVE player2_move = NONE;
 
-Vector2 player_p = { 0.0f, 0.0f };
+struct Player { Vector2 pos; enum PLAYER_MOVE dir; };
+
+struct Player player1 = { .pos = { 0.0f, 0.0f }, .dir = NONE };
+struct Player player2 = { .pos = { 0.0f, 0.0f }, .dir = NONE };
 
 Vector2 ball_p = { 0.0f, 0.0f };
 Vector2 ball_v = { 0.0f, 0.0f };
@@ -46,19 +51,19 @@ bool ball_is_out = false;
 /* update fns */
 
 
-void update_player(float dt)
+void update_player(struct Player *player, float dt)
 {
-    switch (player_move) {
+    switch (player->dir) {
         case UP:
-            player_p.y -= paddle_speed * dt;
+            player->pos.y -= paddle_speed * dt;
             break;
         case DOWN:
-            player_p.y += paddle_speed * dt;
+            player->pos.y += paddle_speed * dt;
             break;
         default:
             break;
     }
-    player_p.y = Clamp(player_p.y, 0, window_h - paddle_h);
+    player->pos.y = Clamp(player->pos.y, 0, window_h - paddle_h);
 }
 
 
@@ -73,18 +78,23 @@ void update_ball(float dt)
 
 void update_collisions(void)
 {
-    if ((ball_p.y < player_p.y) || (ball_p.y > player_p.y + paddle_h)) return;
-    if (ball_p.x > player_p.x + paddle_w + ball_r) return;
-    ball_v.x *= -1;
+    if (ball_p.x <= player1.pos.x + paddle_w + ball_r) {
+        if ((ball_p.y < player1.pos.y) || (ball_p.y > player1.pos.y + paddle_h)) return;
+        ball_v.x *= -1;
+    }
+    if (ball_p.x >= player2.pos.x - ball_r) {
+        if ((ball_p.y < player2.pos.y) || (ball_p.y > player2.pos.y + paddle_h)) return;
+        ball_v.x *= -1;
+    }
 }
 
 
 /* draw fns */
 
 
-void draw_paddle(Vector2 p)
+void draw_player(struct Player *player)
 {
-    DrawRectangle(p.x, p.y, paddle_w, paddle_h, WHITE);
+    DrawRectangle(player->pos.x, player->pos.y, paddle_w, paddle_h, WHITE);
 }
 
 
@@ -99,13 +109,19 @@ void draw_ball(void)
 
 void pong_reset(void)
 {
-    static int seed = 0;
     ball_is_out = false;
 
-    player_p = (Vector2) { 6.0f, (window_h - paddle_h) / 2 };
+    player1 = (struct Player) {
+        .pos = (Vector2) { 6.0f, (window_h - paddle_h) / 2 },
+        .dir = NONE
+    };
+    player2 = (struct Player) {
+        .pos = (Vector2) { (window_w - 6.0f - paddle_w), (window_h - paddle_h) / 2 },
+        .dir = NONE
+    };
+
     ball_p = (Vector2){ window_w / 2, window_h / 2 };
 
-    SetRandomSeed(seed++);
     float theta = GetRandomValue(0, 360);
     ball_v = (Vector2){
         ball_speed * cosf(theta),
@@ -119,9 +135,9 @@ void input_parse(void)
     bool down = IsKeyDown(KEY_DOWN), up = IsKeyDown(KEY_UP);
 
     if ((down && up) || (!down && !up)) {
-        player_move = NONE;
+        player1.dir = NONE;
     } else {
-        player_move = (down) ? DOWN : UP;
+        player1.dir = (down) ? DOWN : UP;
     }
 }
 
@@ -130,7 +146,8 @@ void pong_update()
 {
     float dt = GetFrameTime();
     input_parse();
-    update_player(dt);
+    update_player(&player1, dt);
+    update_player(&player2, dt);
     update_ball(dt);
 
     update_collisions();
@@ -143,7 +160,8 @@ void pong_draw(void)
     BeginDrawing();
     ClearBackground(SKYBLUE);
 
-    draw_paddle(player_p);
+    draw_player(&player1);
+    draw_player(&player2);
     draw_ball();
 
     EndDrawing();
@@ -154,6 +172,7 @@ void pong_initialise(void)
 {
     InitWindow(window_w, window_h, "pong");
     SetExitKey(KEY_Q);
+    SetRandomSeed(1 + GetMouseX()*GetMouseX() + GetMouseY()*GetMouseY());
     pong_reset();
 }
 

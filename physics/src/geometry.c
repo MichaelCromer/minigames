0004c11596e0fcfb3edeb9c24f441a257fdb8b03
Michael Cromer <mcromer@proton.me>
2026-03-18 16:03:46 +1100

    Update geometry with intersections

--- physics/src/geometry.c
+++ physics/src/geometry.c
@@ -2,31 +2,57 @@
 #include <raymath.h>
 #include <stddef.h>
 
-struct Point { float x, float y };
-struct Segment { Vector2 v0; Vector2 v1; };
-struct Triangle { Vector2 v0; Vector2 v1; Vector2 v2; };
-struct Polygon { size_t n, Vector2 *v };
+
+struct Point { float x; float y; };
+struct Segment { struct Point p0; struct Point p1; };
+struct Triangle { struct Point p0; struct Point p1; struct Point p2; };
+struct Polygon { size_t n; struct Point *p; };
+
 
 /*
- *  VECTOR
+ *  POINT/VECTOR
  */
 
 
-float vector2_dot(const Vector2 v1, const Vector2 v2)
+float vector_len_sqr(const struct Point p)
 {
-    return (v1.x*v2.x) + (v1.y*v2.y);
+    return (p.x*p.x) + (p.y*p.y);
 }
 
 
-float vector2_cross(const Vector2 v1, const Vector2 v2)
+float vector_dot(const struct Point p1, const struct Point p2)
 {
-    return (v1.x*v2.y) - (v1.y*v2.x);
+    return (p1.x*p2.x) + (p1.y*p2.y);
 }
 
 
-Vector2 vector2_perp(const Vector2 v)
+float vector_cross(const struct Point p1, const struct Point p2)
 {
-    return (Vector2) { -v.y, v.x };
+    return (p1.x*p2.y) - (p1.y*p2.x);
+}
+
+
+struct Point vector_perp(const struct Point v)
+{
+    return (struct Point) { -v.y, v.x };
+}
+
+
+struct Point vector_scale(const struct Point p, const float s)
+{
+    return (struct Point) { s*p.x, s*p.y };
+}
+
+
+struct Point vector_sum(const struct Point p1, const struct Point p2)
+{
+    return (struct Point) { p1.x + p2.x, p1.y + p2.y };
+}
+
+
+struct Point vector_diff(const struct Point p1, const struct Point p2)
+{
+    return (struct Point) { p1.x - p2.x, p1.y - p2.y };
 }
 
 
@@ -38,15 +64,15 @@ Vector2 vector2_perp(const Vector2 v)
 float triangle_area(const struct Triangle t)
 {
     return fabsf(
-        vector2_cross(Vector2Subtract(t.v1, t.v0), Vector2Subtract(t.v2, t.v0))
+        vector_cross(vector_diff(t.p1, t.p0), vector_diff(t.p2, t.p0))
     );
 }
 
 
-struct Triangle triangle_translate(const struct Triangle t, const Vector2 v)
+struct Triangle triangle_translate(const struct Triangle t, const struct Point v)
 {
     return (struct Triangle) {
-        Vector2Add(t.v0, v), Vector2Add(t.v1, v), Vector2Add(t.v2, v)
+        vector_sum(t.p0, v), vector_sum(t.p1, v), vector_sum(t.p2, v)
     };
 }
 
@@ -55,49 +81,48 @@ struct Triangle triangle_translate(const struct Triangle t, const Vector2 v)
  * POLYGON
  */
 
-float polygon_area(const struct Polygon p)
+
+float polygon_area_moment_0(const struct Polygon p)
 {
-    float area = 0;
-    for (size_t i = 0; i < p.n; i++) { area += vector2_cross(p.v[i], p.v[(i+1) % p.n]) }
-    return 0.5 * fabsf(area);
+    float m0 = 0;
+    for (size_t i = 0; i < p.n; i++) { m0 += vector_cross(p.p[i], p.p[(i+1) % p.n]); }
+    return 0.5f * fabsf(m0);
 }
 
 
-Vector2 polygon_centroid(const struct Polygon p)
+struct Point polygon_area_moment_1(const struct Polygon p)
 {
-    Vector2 centroid = { 0 };
+    float factor = 0, denom = 0;
+    struct Point m1 = { 0 };
+    struct Point curr = { 0 }, next = p.p[0];
 
-    float denom = 0;
-    Vector2 curr = { 0 }, p.v[0];
     for (size_t i = 0; i < p.n; i++) {
-        curr = next, next = p.v[(i+1) % p.n];
-        factor = vector2_cross(curr, next);
-        centroid = Vector2Add(centroid, (Vector2Scale(Vector2Add(curr, next), factor)));
+        curr = next, next = p.p[(i+1) % p.n];
+        factor = vector_cross(curr, next);
+        m1 = vector_sum(m1, (vector_scale(vector_sum(curr, next), factor)));
         denom += factor;
     }
 
-    return Vector2Scale(centroid, 1/(3.0f*denom));
+    return vector_scale(m1, 1/(3.0f*denom));
 }
 
 
-float polygon_area_moment_2(Vector2 *vertices, size_t n)
+float polygon_area_moment_2(const struct Polygon p)
 {
-    if (!vertices || !n) return 0;
+    float m2 = 0;
+    float factor = 0, denom = 0;
+    struct Point curr = { 0 }, next = p.p[0];
 
-    float moment_2 = 0;
-    float factor = 0, denominator = 0;
-    Vector2 curr = { 0 }, next = vertices[0];
-
-    for (size_t i = 0; i < n; i++) {
-        curr = next, next = vertices[(i+1) % n];
-        factor = vector2_cross(curr, next);
-        moment_2 += factor * (
-            Vector2LengthSqr(curr) + vector2_dot(curr, next) + Vector2LengthSqr(next)
+    for (size_t i = 0; i < p.n; i++) {
+        curr = next, next = p.p[(i+1) % p.n];
+        factor = vector_cross(curr, next);
+        m2 += factor * (
+            vector_len_sqr(curr) + vector_dot(curr, next) + vector_len_sqr(next)
         );
-        denominator += factor;
+        denom += factor;
     }
     
-    return moment_2 / (6*denominator);
+    return m2 / (6.0f*denom);
 }
 
 
@@ -106,30 +131,44 @@ float polygon_area_moment_2(Vector2 *vertices, size_t n)
  */
 
 
-bool is_point_on_triangle(const Vector2 p, const struct Triangle t, const float eps)
+bool is_point_on_triangle(const struct Point p, const struct Triangle t, const float eps)
 {
-    const Vector2 dt1 = Vector2Subtract(t.v1, t.v0);
-    const Vector2 dt2 = Vector2Subtract(t.v2, t.v0);
-    const Vector2 dp = Vector2Subtract(p, t.v0);
+    /*
+     * need to check if coordinates given by a(t1 - t0) + b(t2 - t0) = p - t0
+     * are within bounds (i.e. have a > 0 and b > 0 and a + b < 1)
+     *
+     *  dt1 = (t1 - t0),    dt2 = (t2 - t0),    dp = (p - t0)
+     *
+     * get
+     *  a(dt1 x dt2) = dp x dt2,    b(dt1 x dt2) = dt1 x dp
+     *
+     *  check requires matching sign! i.e. dt1 x dt2 may be negative
+     */
 
-    const float det = fabsf(vector2_cross(dt1, dt2));
-    const float x = fabsf(vector2_cross(dp, dt2));
-    const float y = fabsf(vector2_cross(dt1, dp));
+    const struct Point dt1 = vector_diff(t.p1, t.p0);
+    const struct Point dt2 = vector_diff(t.p2, t.p0);
+    const struct Point dp = vector_diff(p, t.p0);
+
+    const float det = vector_cross(dt1, dt2);
+    const float x = vector_cross(dp, dt2);
+    const float y = vector_cross(dt1, dp);
 
     return (
-        (det > EPSILON) && (x > -eps*det) && (y > -eps*det) && ((x+y) < (1+eps)*det)
+        ((det > 0) && ((x > -2*eps) && (y > -2*eps) && ((x + y) < (1+2*eps)*det)))
+            ||
+        ((det < 0) && ((x < 2*eps) && (y < 2*eps) && ((x + y) > (1+2*eps)*det)))
     );
 }
 
 
-bool is_point_on_segment(const Vector2 p, const struct Segment s, const float eps)
+bool is_point_on_segment(const struct Point p, const struct Segment s, const float eps)
 {
-    const Vector2 ds = Vector2Subtract(s.v1, s.v0);
-    const Vector2 dp = Vector2Subtract(p, s.v0);
+    const struct Point ds = vector_diff(s.p1, s.p0);
+    const struct Point dp = vector_diff(p, s.p0);
 
-    const float det = Vector2LengthSqr(ds);
-    const float x = vector2_dot(dp, ds);
-    const float y = fabsf(vector2_cross(dp, ds));
+    const float det = vector_len_sqr(ds);
+    const float x = vector_dot(dp, ds);
+    const float y = fabsf(vector_cross(dp, ds));
 
     return (
         (det > EPSILON) && (x > -eps*det) && (x < (1 + eps)*det) && (y < eps*det)
@@ -137,77 +176,36 @@ bool is_point_on_segment(const Vector2 p, const struct Segment s, const float ep
 }
 
 
-bool is_segment_on_segment(const struct Segment s1, const struct Segment s2)
-{
-    /* solve the system (K : p0 + a*dp) == (L : q0 + b*dq)
-     * check that the solution has a, b in [0, 1]
-     */
-
-    const Vector2
-
-    Vector2 dp = Vector2Subtract(p1, p0);
-    Vector2 dq = Vector2Subtract(q1, q0);
-    Vector2 r = Vector2Subtract(q0, p0);
-
-    float s = vector2_cross(r, dq);
-    float t = vector2_cross(r, dp);
-    float u = vector2_cross(dp, dq);
-    
-    if (u < 0) { s = -s, t = -t, u = -u; }
-    return ((s >= 0) && (t >= 0) && (s <= u) && (t <= u));
-}
-
-
-bool is_polygon_is_axis_separate
+bool is_segment_on_segment
 (
-    Vector2 *vertices1, size_t n1, Vector2 *vertices2, size_t n2
+    const struct Segment s1, const struct Segment s2, const float eps
 )
-{    
-    /*  separating axis theorem
-     *      two polygons intersect iff every edge-normal projection intersects
-     */
-    Vector2 curr, next, norm;
-    float min, max, base, proj;
-    bool intersect;
+{
+    const struct Point m1 = vector_sum(s1.p0, s1.p1);
+    const struct Point m2 = vector_sum(s2.p0, s2.p1);
 
-    curr = (Vector2) { 0, 0 }, next = vertices1[0];
-    for (size_t i = 0; i < n1; i++) {
-        intersect = false; 
+    const struct Point ds1 = vector_diff(s1.p1, s1.p0);
+    const struct Point ds2 = vector_diff(s2.p1, s2.p0);
+    const struct Point d12 = vector_diff(m2, m1);
 
-        curr = next, next = vertices1[(i+1) % n1];
-        norm = vector2_perp(Vector2Subtract(next, curr));
+    const float u = fabsf(vector_cross(d12, ds2));
+    const float v = fabsf(vector_cross(d12, ds1));
+    const float w = fabsf(vector_cross(ds2, ds1));
 
-        /* base axis *min* assuming anticlockwise points */
-        base = vector2_dot(norm, curr);
-
-        proj = vector2_dot(norm, vertices2[0]);
-        min = proj, max = proj;
-        for (size_t j = 0; j < n2; j++) {
-            proj = vector2_dot(norm, vertices2[j]);
-
-            if (proj < min) min = proj;
-            if (proj > max) max = proj;
-
-            /* we know intersection as soon as max and min are on opposite sides */
-            if (((min < base) && (max > base)) || ((min > base) && (max < base))) {
-                intersect = true;
-                break;
-            }
-        }
-
-        if (!intersect) return false;
-    }
-    return true;
+    return ((u <= (1 + eps)*w) && (v <= (1 + eps)*w));
 }
 
 
-bool is_polygon_on_polygon
+bool is_segment_on_triangle
 (
-    Vector2 *vertices1, size_t n1, Vector2 *vertices2, size_t n2
+    const struct Segment s, const struct Triangle t, const float eps
 )
 {
     return (
-        polygon_is_axis_separate(vertices1, n1, vertices2, n2) ||
-        polygon_is_axis_separate(vertices2, n2, vertices1, n1)
-    );
+        is_segment_on_segment(s, (struct Segment) { t.p0, t.p1 }, eps)
+            ||
+        is_segment_on_segment(s, (struct Segment) { t.p1, t.p2 }, eps)
+            ||
+        is_segment_on_segment(s, (struct Segment) { t.p2, t.p0 }, eps)
+   );
 }

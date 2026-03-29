f95496c40e61c467c9913a584ae8581870c19b66
Michael Cromer <mcromer@proton.me>
2026-03-30 08:05:55 +1100

    Begin rigidbodyarena demo

--- physics/demo/arena.c
+++ physics/demo/arena.c
@@ -1,6 +1,64 @@
+#include <raylib.h>
+#include <raymath.h>
+#include <stdlib.h>
+
 #include "../src/arena.c"
+
+
+const int win_width = 800;
+const int win_height = 600;
+const char* win_title = "Physics Simulation Test : Rigid Body";
+
+
+const size_t N_BODY = 1;
+struct RigidBodyArena *rba = NULL;
+
+struct Body *b = NULL;
+
+
+void body_randominse(void)
+{
+
+}
+
+
+void initialise(void)
+{
+    rba = rigidbodyarena_create(N_BODY);
+}
+
+
+void deinitialise(void)
+{
+    rigidbodyarena_destroy(rba);
+}
+
+
+void draw(void)
+{
+    BeginDrawing();
+    {
+        /* stuff */
+    }
+    EndDrawing();
+}
+
+
+void update(void)
+{
+    return;
+}
+
 
 int main(void)
 {
+    initialise();
+
+    while (!WindowShouldClose()) {
+        draw();
+        update();
+    }
+
+    deinitialise();
     return 0;
 }
